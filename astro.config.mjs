784b09f84fdf5754d0cd658ed1d43110e2d389b2
Dylan65 <dylanjesus65@gmail.com>
2025-04-26 03:10:41 -0500

    CambiosVistav4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import preact from "@astrojs/preact"; // 👈 Importa el renderizador
+import Swiper from 'swiper';
 
 // https://astro.build/config
 export default defineConfig({
